e48463562b0017ded0f607f090066ea6da312663
Luis_Garcia <lufergape02@hotmail.com>
2024-09-11 15:04:14 -0500

    content

--- src/Components/Dashboard/country/country_list.js
+++ src/Components/Dashboard/country/country_list.js
@@ -5,7 +5,13 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import CardActionArea from '@mui/material/CardActionArea';
+import Dialog from '@mui/material/Dialog';
+import DialogContent from '@mui/material/DialogContent';
+import DialogTitle from '@mui/material/DialogTitle';
+import IconButton from '@mui/material/IconButton';
+import { Icon } from '@iconify/react';
 import styles from "@/app/page.module.css";
+import { styled } from '@mui/material/styles';
 import { gql, useQuery } from '@apollo/client';
 
 const API_KEY = '45919412-b9b196e0740f044b87417a48f';
@@ -15,10 +21,17 @@ const GETCOUNTRIES = gql`
         countries {
             name
             capital
-            continent {
+            continent{
                 name
             }
             emoji
+            languages{
+                name
+            }
+            currencies
+            subdivisions{
+                name
+            }
         }
     }
 `;
@@ -31,9 +44,17 @@ const CardArray = (array, size) => {
     return list;
 };
 
+const CustomDialog = styled(Dialog)(({ theme }) => ({
+    '& .MuiBackdrop-root': {
+        backgroundColor: 'transparent',
+    },
+}));
+
 export function CountryList({ filterText, continentFilter }) {
     const { data, error, loading } = useQuery(GETCOUNTRIES);
     const [images, setImages] = useState({});
+    const [selectedCountry, setSelectedCountry] = useState(null);
+    const [openModal, setOpenModal] = useState(false);
 
     useEffect(() => {
         if (data && data.countries) {
@@ -73,12 +94,22 @@ export function CountryList({ filterText, continentFilter }) {
 
     const listCard = CardArray(filteredCountries, 3);
 
+    const handleCardClick = (country) => {
+        setSelectedCountry(country);
+        setOpenModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setOpenModal(false);
+        setSelectedCountry(null);
+    };
+
     return (
         <>
             {listCard.map((list, listIndex) => (
                 <div className={styles.d_car} key={listIndex}>
                     {list.map((card, index) => (
-                        <Card key={index} sx={{ maxWidth: 345, minWidth: 340, borderRadius: '20px 20px 0 0' }}>
+                        <Card key={index} sx={{ maxWidth: 345, minWidth: 340, borderRadius: '20px 20px 0 0' }} onClick={() => handleCardClick(card)}>
                             <CardActionArea>
                                 <CardMedia
                                     component="img"
@@ -105,6 +136,68 @@ export function CountryList({ filterText, continentFilter }) {
                     ))}
                 </div>
             ))}
+
+            <CustomDialog open={openModal} onClose={handleCloseModal} maxWidth="sm" fullWidth sx={{ left: "70%", borderRadius: "20px" }}>
+                <DialogTitle>
+                    <div style={{ fontSize: '30px', marginRight: '10px', paddingLeft: "15px", display: "flex", gap: "20px" }}>
+                        <div style={{ fontSize: '40px', paddingLeft: "15px", display: "flex"}}>
+                        {selectedCountry?.emoji}
+                        </div>
+                        <div style={{ fontSize: '40px', marginRight: '2px', paddingLeft: "2px" }}>
+                        {selectedCountry?.name}
+                        </div>
+                    </div>
+                    
+                    <IconButton
+                        edge="end"
+                        color="inherit"
+                        onClick={handleCloseModal}
+                        aria-label="close"
+                        sx={{
+                            position: 'absolute',
+                            right: 8,
+                            top: 8,
+                            color: (theme) => theme.palette.grey[500],
+                        }}
+                    >
+                        <Icon icon="heroicons-solid:x" />
+                    </IconButton>
+                </DialogTitle>
+                <DialogContent>
+                    {selectedCountry && (
+                        <div>
+                            <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr', gap: '10px' }}>
+                                <Typography variant="h6" style={{display:"flex", gap:"20px" }}>
+                                    <p style={{ color: "#009CFF" }}>Capital:</p> {selectedCountry.capital}
+                                </Typography>
+                                <Typography variant="h6" style={{display:"flex", gap:"20px" }}>
+                                    <p style={{ color: "#009CFF" }}>Continent:</p> {selectedCountry.continent.name}
+                                </Typography>
+
+                                <Typography variant="h6" style={{display:"flex", gap:"20px" }}> 
+                                    <p style={{ color: "#009CFF" }}>Language:</p> {selectedCountry.languages.map(lang => lang.name).join(', ')}
+                                </Typography>
+                                <Typography variant="h6" style={{display:"flex", gap:"20px" }}>
+                                    <p style={{ color: "#009CFF" }}>Currency:</p> {selectedCountry.currencies}
+                                </Typography>
+                            </div>
+                            <Card sx={{ mt: 2, maxHeight: '200px', overflowY: 'auto', borderRadius: "15px", borderColor: "#009CFF", border: "15px" }}>
+                                <CardContent>
+                                    <Typography variant="h6">Region:</Typography>
+                                    {selectedCountry.subdivisions.map((sub, index) => (
+                                        <Typography key={index} variant="body2">- {sub.name}</Typography>
+                                    ))}
+                                </CardContent>
+                            </Card>
+                            <img
+                                src={images[selectedCountry.name] || ''}
+                                alt={selectedCountry.name}
+                                style={{ width: '100%', marginTop: '10px' }}
+                            />
+                        </div>
+                    )}
+                </DialogContent>
+            </CustomDialog>
         </>
     );
 }
